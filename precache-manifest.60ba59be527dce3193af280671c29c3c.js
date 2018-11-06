340324ed661f9aff112843d17616f41f9dcb079e
Nans <nans.gigan@gmail.com>
2018-11-06 23:20:58 +0100

    Updates

---
+++ precache-manifest.60ba59be527dce3193af280671c29c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-poc/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e86a8ef6a55c75f99ac0",
+    "url": "/react-deploy-poc/static/js/runtime~main.e86a8ef6.js"
+  },
+  {
+    "revision": "69c6839c42cf24aa8a26",
+    "url": "/react-deploy-poc/static/js/main.69c6839c.chunk.js"
+  },
+  {
+    "revision": "b4c0028c3d2a9f178a8f",
+    "url": "/react-deploy-poc/static/js/1.b4c0028c.chunk.js"
+  },
+  {
+    "revision": "69c6839c42cf24aa8a26",
+    "url": "/react-deploy-poc/static/css/main.2102c69a.chunk.css"
+  },
+  {
+    "revision": "1531be7f701b3412ea63e5d3b3df5e65",
+    "url": "/react-deploy-poc/index.html"
+  }
+];
